355499cdaffbea5007f891bab8c97244ef25bfae
Ilija Lazoroski <ilija.la@live.com>
2024-06-21 18:20:21 +0000

    Fix type hints in compare_package_lists

--- agent_plugin_builder/compare_package_lists.py
+++ agent_plugin_builder/compare_package_lists.py
@@ -1,6 +1,7 @@
 import json
 from itertools import groupby, islice
 from pathlib import Path
+from typing import Any, Iterable
 
 
 def load_package_names(file_path: Path) -> set[str]:
@@ -9,11 +10,11 @@ def load_package_names(file_path: Path) -> set[str]:
         return {p["download_info"]["url"].split("/")[-1] for p in packages_dict["install"]}
 
 
-def take(n: int, iterable) -> list[str]:
+def take(n: int, iterable: Iterable[Any]) -> list[str]:
     "Return first n items of the iterable as a list."
     return list(islice(iterable, n))
 
 
-def all_equal(packages: list[set[str]]) -> bool:
+def all_equal(packages: Iterable[Any]) -> bool:
     "Returns True if all the elements are equal to each other."
     return len(take(2, groupby(packages, None))) <= 1
