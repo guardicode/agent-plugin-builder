83439fbc912913c184197ba3cd09e816a1437c67
Kekoa Kaaikala <kekoa.kaaikala@gmail.com>
2024-06-25 18:08:37 +0000

    Add infectionmonkey prefix to image names

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 SCRIPT_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
 
+WINE_DOCKER_TAG=infectionmonkey/wine-docker:devel
+PYWINE_TAG=infectionmonkey/pywine:3.11
+
 cd "$SCRIPT_DIR"/wine-docker || exit
-docker build --platform linux/amd64 -t wine-docker:devel --build-arg WINE_FLAVOUR=devel .
+docker build --platform linux/amd64 -t $WINE_DOCKER_TAG --build-arg WINE_FLAVOUR=devel .
 
 cd "$SCRIPT_DIR"/pywine || exit
-sed 's|FROM tobix/wine:devel|FROM wine-docker:devel|' Dockerfile > Dockerfile.modified
-docker build --platform linux/amd64 -t pywine:3.11 --build-arg PYTHON_VERSION=3.11.9 -f Dockerfile.modified .
+sed "s|FROM tobix/wine:devel|FROM $WINE_DOCKER_TAG|" Dockerfile > Dockerfile.modified
+docker build --platform linux/amd64 -t $PYWINE_TAG --build-arg PYTHON_VERSION=3.11.9 -f Dockerfile.modified .
 rm Dockerfile.modified
 
 cd "$SCRIPT_DIR" || exit
