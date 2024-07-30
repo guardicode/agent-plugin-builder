4e07217841f621e05c13c7853bf4c8ffd44ec64c
Ilija Lazoroski <ilija.la@live.com>
2024-07-30 09:50:31 +0000

    Add integration tests for plugin_archive_generation

--- tests/test_plugin_archive_generation.py
+++ tests/test_plugin_archive_generation.py
@@ -1,10 +1,9 @@
+import shutil
 import tarfile
 from pathlib import Path
 from unittest.mock import MagicMock
 
 import pytest
-from monkeytypes import AgentPluginManifest, AgentPluginType, OperatingSystem
-
 from agent_plugin_builder import (
     AgentPluginBuildOptions,
     PlatformDependencyPackagingMethod,
@@ -15,6 +14,7 @@ from agent_plugin_builder import (
 from agent_plugin_builder.plugin_archive_generation import EXCLUDE_SOURCE_FILES, SOURCE
 from agent_plugin_builder.plugin_manifest import MANIFEST
 from agent_plugin_builder.plugin_schema_generation import CONFIG_SCHEMA
+from monkeytypes import AgentPluginManifest, AgentPluginType, OperatingSystem
 
 AGENT_PLUGIN_MANIFEST = AgentPluginManifest(
     name="Plugin",
@@ -27,8 +27,20 @@ AGENT_PLUGIN_MANIFEST = AgentPluginManifest(
     link_to_documentation="https://plugin_documentation.com",
     safe=True,
 )
+TEST_SOURCE_DIR_NAME = "test_source_dir"
+TEST_BUILD_DIR_NAME = "test_build_dir"
 
 PLUGIN_ARCHIVE_NAME = "Plugin-exploiter.tar"
+MOCK_SOURCE_DIR_NAME = "mock_exploiter"
+MOCK_AGENT_PLUGIN_MANIFEST = AgentPluginManifest(
+    name="Mock",
+    plugin_type=AgentPluginType.EXPLOITER,
+    supported_operating_systems=(OperatingSystem.LINUX,),
+    target_operating_systems=(OperatingSystem.LINUX,),
+    title="Mock Exploiter",
+    version="0.1.0",
+    safe=True,
+)
 
 
 def list_tar_contents(tar_gz_path):
@@ -47,7 +59,7 @@ def agent_plugin_build_options(tmpdir):
     temp_dir = Path(tmpdir)
     plugin_dir_path = temp_dir / "plugin_dir_path"
     plugin_dir_path.mkdir()
-    build_dir_path = temp_dir / "build_dir_path"
+    build_dir_path = temp_dir / TEST_BUILD_DIR_NAME
     build_dir_path.mkdir()
     dist_dir_path = temp_dir / "dist_dir_path"
     dist_dir_path.mkdir()
@@ -56,10 +68,100 @@ def agent_plugin_build_options(tmpdir):
         plugin_dir_path=plugin_dir_path,
         build_dir_path=build_dir_path,
         dist_dir_path=dist_dir_path,
-        source_dir_name="source_dir_name",
+        source_dir_name=TEST_SOURCE_DIR_NAME,
         platform_dependencies=PlatformDependencyPackagingMethod.COMMON,
         verify_hashes=False,
     )
+
+
+@pytest.fixture
+def agent_plugin_build_options_plugin(tmpdir, data_for_tests_dir):
+    plugin_dir_path_data = data_for_tests_dir / "mock-exploiter"
+    plugin_dir_path = Path(tmpdir) / "mock-exploiter"
+    plugin_dir_path.mkdir()
+    shutil.copytree(plugin_dir_path_data, plugin_dir_path, dirs_exist_ok=True)
+    build_dir_path = plugin_dir_path / "build"
+    build_dir_path.mkdir()
+    shutil.copytree(plugin_dir_path, build_dir_path, dirs_exist_ok=True)
+    dist_dir_path = plugin_dir_path / "dist"
+    dist_dir_path.mkdir()
+
+    def make_agent_plugin_build_options(platform_dependencies):
+        return AgentPluginBuildOptions(
+            plugin_dir_path=plugin_dir_path,
+            build_dir_path=build_dir_path,
+            dist_dir_path=dist_dir_path,
+            source_dir_name=MOCK_SOURCE_DIR_NAME,
+            platform_dependencies=platform_dependencies,
+            verify_hashes=False,
+        )
+
+    return make_agent_plugin_build_options
+
+
+@pytest.mark.parametrize(
+    "platform_dependencies, expected_source_tar_contents",
+    [
+        (
+            PlatformDependencyPackagingMethod.COMMON,
+            [
+                "vendor",
+                "__init__.py",
+                "plugin.py",
+                "mock_options.py",
+            ],
+        ),
+        (
+            PlatformDependencyPackagingMethod.SEPARATE,
+            [
+                "vendor-linux",
+                "__init__.py",
+                "plugin.py",
+                "mock_options.py",
+            ],
+        ),
+        # TODO: Uncomment this when the TODO in vendor_dir_generation is addressed
+        #        (
+        #            PlatformDependencyPackagingMethod.AUTODETECT,
+        #            [
+        #                "vendor-linux",
+        #                "__init__.py",
+        #                "plugin.py",
+        #                "mock_options.py",
+        #            ],
+        #        ),
+    ],
+)
+@pytest.mark.integration
+def test_create_agent_plugin_archive(
+    agent_plugin_build_options_plugin, platform_dependencies, expected_source_tar_contents
+):
+    agent_plugin_build_options = agent_plugin_build_options_plugin(platform_dependencies)
+
+    create_agent_plugin_archive(agent_plugin_build_options, MOCK_AGENT_PLUGIN_MANIFEST)
+
+    source_archive_path = agent_plugin_build_options.build_dir_path / f"{SOURCE}.tar.gz"
+    plugin_archive_path = agent_plugin_build_options.dist_dir_path / "Mock-exploiter.tar"
+    assert list_tar_contents(source_archive_path) == expected_source_tar_contents
+    assert plugin_archive_path.exists()
+    assert list_tar_contents(plugin_archive_path) == [
+        f"{SOURCE}.tar.gz",
+        CONFIG_SCHEMA,
+        f"{MANIFEST}.yaml",
+    ]
+
+
+@pytest.mark.integration
+def test_create_agent_plugin_archive__dist_dir_created(agent_plugin_build_options_plugin):
+    agent_plugin_build_options = agent_plugin_build_options_plugin(
+        PlatformDependencyPackagingMethod.COMMON
+    )
+    agent_plugin_build_options.dist_dir_path.rmdir()
+
+    create_agent_plugin_archive(agent_plugin_build_options, MOCK_AGENT_PLUGIN_MANIFEST)
+
+    assert agent_plugin_build_options.dist_dir_path.exists()
+    assert agent_plugin_build_options.dist_dir_path.is_dir()
 
 
 def test_create_agent_plugin_archive__empty_plugin(monkeypatch, agent_plugin_build_options):
@@ -69,17 +171,16 @@ def test_create_agent_plugin_archive__empty_plugin(monkeypatch, agent_plugin_bui
 
 def test_create_source_archive(tmpdir):
     temp_dir = Path(tmpdir)
-    build_dir_path = temp_dir / "build_dir_path"
+    build_dir_path = temp_dir / TEST_BUILD_DIR_NAME
     build_dir_path.mkdir()
-    source_dir_name = "source_dir_name"
-    source_dir_path = build_dir_path / source_dir_name
+    source_dir_path = build_dir_path / TEST_SOURCE_DIR_NAME
     source_dir_path.mkdir()
 
     (source_dir_path / "testfile.py").touch()
     for exclude_file in EXCLUDE_SOURCE_FILES:
         (source_dir_path / exclude_file).touch()
 
-    source_archive_path = create_source_archive(build_dir_path, source_dir_name)
+    source_archive_path = create_source_archive(build_dir_path, TEST_SOURCE_DIR_NAME)
 
     assert source_archive_path.exists()
     assert source_archive_path.name == f"{SOURCE}.tar.gz"
@@ -90,7 +191,7 @@ def test_create_source_archive(tmpdir):
 
 def test_create_plugin_archive(tmpdir):
     temp_dir = Path(tmpdir)
-    build_dir_path = temp_dir / "build_dir_path"
+    build_dir_path = temp_dir / TEST_BUILD_DIR_NAME
     build_dir_path.mkdir()
     (build_dir_path / CONFIG_SCHEMA).touch()
     source_archive_path = build_dir_path / f"{SOURCE}.tar.gz"
@@ -112,22 +213,21 @@ def test_create_plugin_archive(tmpdir):
 def test_create_source_archive__os_error(monkeypatch, tmpdir):
     monkeypatch.setattr(tarfile, "open", MagicMock(side_effect=OSError("Test OSError")))
     temp_dir = Path(tmpdir)
-    build_dir_path = temp_dir / "build_dir_path"
+    build_dir_path = temp_dir / TEST_BUILD_DIR_NAME
     build_dir_path.mkdir()
-    source_dir_name = "source_dir_name"
-    source_dir_path = build_dir_path / source_dir_name
+    source_dir_path = build_dir_path / TEST_SOURCE_DIR_NAME
     source_dir_path.mkdir()
 
     (source_dir_path / "testfile.py").touch()
 
     with pytest.raises(OSError):
-        create_source_archive(build_dir_path, source_dir_name)
+        create_source_archive(build_dir_path, TEST_SOURCE_DIR_NAME)
 
 
 def test_create_plugin_archive__removes_existing_archive(monkeypatch, tmpdir):
     monkeypatch.setattr(tarfile, "open", MagicMock())
     temp_dir = Path(tmpdir)
-    build_dir_path = temp_dir / "build_dir_path"
+    build_dir_path = temp_dir / TEST_BUILD_DIR_NAME
     build_dir_path.mkdir()
     (build_dir_path / PLUGIN_ARCHIVE_NAME).touch()
 
